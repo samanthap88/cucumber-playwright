a5041442554706e970910fddcd026aec6be59d82
samantha <samanthap8675@gmail.com>
2023-09-19 17:03:06 -0400

    auto read files

--- example.spec.ts
+++ example.spec.ts
@@ -4,12 +4,19 @@ import { test } from '@playwright/test';
 
 import generateStepDefinitions from './codeGenerator';
 import fs from 'fs';
+const path = require('path');
 
-const featureFileContent = fs.readFileSync('example.feature', 'utf-8');
+let featureFileContent ; 
+const featureFiles = fs.readdirSync('features/') ; 
+
+
+featureFiles.forEach(element => {
+  const filePath = path.join('features/', element);
+  featureFileContent = fs.readFileSync(filePath, 'utf-8');
+});
 
 test('Navigate to Google', async ({ page }) => {
-  await generateStepDefinitions(featureFileContent, page);
-
+  await generateStepDefinitions(featureFileContent, page)
  
 });
 
